8628d0be3d471147794ee5f832d306a68f57080b
Manjul Bhattrai <workmanjul@gmail.com>
2023-06-05 15:49:49 +0545

    Update removed files

--- prisma/migrations/20230601045330_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `status` to the `AdAccount` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `AdAccount` ADD COLUMN `status` VARCHAR(191) NOT NULL;
