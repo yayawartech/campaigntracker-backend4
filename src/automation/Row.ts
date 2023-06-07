39b1e1f88fc87f2379ff851e648c9d76da28b621
anantakoirala <ananta.drn2049@gmail.com>
2023-06-07 15:55:55 +0545

    automation crud

---
+++ src/automation/Row.ts
@@ -0,0 +1,13 @@
+export interface Row {
+  id: number;
+  params: string;
+  days: string;
+  operand: string;
+  value: string;
+  types: string;
+  daysAgo: string;
+  daysAgoValue: string;
+  dolorValue: string;
+  daysValue: string;
+  percentValue: string;
+}
